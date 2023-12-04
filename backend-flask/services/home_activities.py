b28c69874a6c86841098c4558b1ce8ad9a3ee027
Bhanu Malhotra <bhanumalhotra219@gmail.com>
2023-12-04 12:17:20 +0000

    added notifications service

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,8 +4,8 @@ class HomeActivities:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Andrew Brown',
-      'message': 'Cloud is fun!',
+      'handle':  'Bhanu Malhotra',
+      'message': 'DevOps is fun!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -14,7 +14,7 @@ class HomeActivities:
       'replies': [{
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Worf',
+        'handle':  'Arshpreet',
         'message': 'This post has no honor!',
         'likes_count': 0,
         'replies_count': 0,
