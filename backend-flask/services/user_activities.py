923cf582fe76d0c75135a6c0d34645bf7192543d
Bhanu Malhotra <bhanumalhotra219@gmail.com>
2023-12-07 21:47:50 +0000

    add segment and subsegment

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,12 +1,16 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
+    segment = xray_recorder.begin_segment('user_activities')
     model = {
       'errors': None,
       'data': None
     }
 
     now = datetime.now(timezone.utc).astimezone()
+
+
 
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
@@ -20,4 +24,10 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
+    dict = {
+      "now": now.isoformat(),
+      "results-size": len(model['data'])
+    }
+    subsegment = xray_recorder.begin_subsegment('mock-data')
+    subsegment.put_metadata('key', dict, 'namespace')  
     return model
