82bd0bc43e58935dc042da42532561c8efa42d90
Bhanu Malhotra <bhanumalhotra219@gmail.com>
2023-12-08 16:13:55 +0000

    cloudwatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -123,7 +123,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(Logger=LOGGER)
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
