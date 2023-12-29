9046d287c198a133307b14ddc953010d6151d8af
Bhanu Malhotra <144083659+bhanumalhotra123@users.noreply.github.com>
2023-12-29 22:49:01 +0000

    fixed cognito, now next we have to add users

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -1,39 +1,47 @@
 import './SigninPage.css';
 import React from "react";
-import {ReactComponent as Logo} from '../components/svg/logo.svg';
+import { ReactComponent as Logo } from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
-
-// [TODO] Authenication
-import Cookies from 'js-cookie'
+import { Auth } from 'aws-amplify';
 
 export default function SigninPage() {
-
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [errors, setErrors] = React.useState('');
 
   const onsubmit = async (event) => {
+    setErrors('');
     event.preventDefault();
-    setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
-      window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
+    try {
+      Auth.signIn(email, password)
+        .then((user) => {
+          localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken);
+          window.location.href = "/";
+        })
+        .catch((error) => {
+          if (error.code === 'UserNotConfirmedException') {
+            window.location.href = "/confirm";
+          }
+          setErrors(error.message);
+        });
+    } // <-- Added closing parenthesis here
+    catch (error) {
+      // Handle other errors here if needed
+      console.error(error);
     }
-    return false
-  }
+    return false;
+  };
 
   const email_onchange = (event) => {
     setEmail(event.target.value);
-  }
+  };
+
   const password_onchange = (event) => {
     setPassword(event.target.value);
-  }
+  };
 
   let el_errors;
-  if (errors){
+  if (errors) {
     el_errors = <div className='errors'>{errors}</div>;
   }
 
@@ -43,7 +51,7 @@ export default function SigninPage() {
         <Logo className='logo' />
       </div>
       <div className='signin-wrapper'>
-        <form 
+        <form
           className='signin_form'
           onSubmit={onsubmit}
         >
@@ -54,7 +62,7 @@ export default function SigninPage() {
               <input
                 type="text"
                 value={email}
-                onChange={email_onchange} 
+                onChange={email_onchange}
               />
             </div>
             <div className='field text_field password'>
@@ -62,7 +70,7 @@ export default function SigninPage() {
               <input
                 type="password"
                 value={password}
-                onChange={password_onchange} 
+                onChange={password_onchange}
               />
             </div>
           </div>
@@ -71,7 +79,6 @@ export default function SigninPage() {
             <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
             <button type='submit'>Sign In</button>
           </div>
-
         </form>
         <div className="dont-have-an-account">
           <span>
@@ -80,7 +87,6 @@ export default function SigninPage() {
           <Link to="/signup">Sign up!</Link>
         </div>
       </div>
-
     </article>
   );
 }
