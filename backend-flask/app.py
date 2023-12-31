02d0769d2276dcbc21eeb71165bfcb35fa75b902
Bhanu Malhotra <144083659+bhanumalhotra123@users.noreply.github.com>
2023-12-31 11:29:43 +0000

    cognito-jwt-server-side-verify

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -30,6 +30,7 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 # CloudWatch Logs ----
 import watchtower
 import logging
+ 
 
 # Rollbar ------
 from time import strftime
@@ -83,8 +84,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 # 
@@ -162,6 +163,10 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 @xray_recorder.capture('activities_home')
 def data_home():
+  app.logger.debug("AUTH HEADER")
+  app.logger.debug(
+    request.headers.get('Authorization')
+  )
   data = HomeActivities.run()  # Logger= LOGGER
   return data, 200
 
