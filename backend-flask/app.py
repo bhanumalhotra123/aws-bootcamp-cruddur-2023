8a96887720bb25b749aea382f5c07dff4e2aa4af
Bhanu Malhotra <bhanumalhotra219@gmail.com>
2024-01-04 22:38:58 +0000

    fixing rollbar for now so that it doesn't cause further err

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -107,7 +107,6 @@ cors = CORS(
 
 # Rollbar ----------
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
 def init_rollbar():
     """init rollbar module"""
     rollbar.init(
