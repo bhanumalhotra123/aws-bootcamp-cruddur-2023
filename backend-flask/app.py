184924de9f504b71200fa178217b3d9146c46890
bhanumalhotra <bhanumalhotra121@gmail.com>
2024-01-19 12:52:50 +0530

    seed for dynamodb and trying name change to my own

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -127,7 +127,7 @@ def rollbar_test():
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'bhanumalhotra'
   model = MessageGroups.run(user_handle=user_handle)
   if model['errors'] is not None:
     return model['errors'], 422
@@ -136,7 +136,7 @@ def data_message_groups():
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
-  user_sender_handle = 'andrewbrown'
+  user_sender_handle = 'bhanumalhotra'
   user_receiver_handle = request.args.get('user_reciever_handle')
 
   model = Messages.run(user_sender_handle=user_sender_handle, user_receiver_handle=user_receiver_handle)
@@ -149,7 +149,7 @@ def data_messages(handle):
 @app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_create_message():
-  user_sender_handle = 'andrewbrown'
+  user_sender_handle = 'bhanumalhotra'
   user_receiver_handle = request.json['user_receiver_handle']
   message = request.json['message']
 
@@ -205,7 +205,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'bhanumalhotra'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
@@ -224,7 +224,7 @@ def data_show_activity(activity_uuid):
 @app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities_reply(activity_uuid):
-  user_handle  = 'andrewbrown'
+  user_handle  = 'bhanumalhotra'
   message = request.json['message']
   model = CreateReply.run(message, user_handle, activity_uuid)
   if model['errors'] is not None:
--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,7 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
+  ('bhanu malhotra', 'bhanumalhotra' ,'MOCK'),
   ('Bhanu Malhotra', 'bhanumalhotra' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
