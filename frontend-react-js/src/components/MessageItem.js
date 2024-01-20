49b1e1ee23bff45661ea1f16ef0123fc1356a05c
bhanumalhotra <bhanumalhotra121@gmail.com>
2024-01-21 04:26:10 +0530

    implement converstaions with dynamodb1

--- frontend-react-js/src/components/MessageItem.js
+++ frontend-react-js/src/components/MessageItem.js
@@ -21,7 +21,7 @@ export default function MessageItem(props) {
   };
 
   return (
-    <Link className='message_item' to={`/messages/@`+props.message.handle}>
+    <Link className='message_item' to={`/messages/@`+props.message_group.uuid}>
       <div className='message_avatar'></div>
       <div className='message_content'>
         <div classsName='message_meta'>
