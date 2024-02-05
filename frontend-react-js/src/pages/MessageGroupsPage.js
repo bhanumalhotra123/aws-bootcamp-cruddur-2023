9135d476a3aa5d27aadffe660317be3bc1e984dd
Bhanu Malhotra <bhanumalhotra219@gmail.com>
2024-02-05 21:29:00 +0000

    refresh token cognito implementation

---
+++ frontend-react-js/src/pages/MessageGroupsPage.js
@@ -0,0 +1,54 @@
+import './MessageGroupsPage.css';
+import React from "react";
+
+import DesktopNavigation  from '../components/DesktopNavigation';
+import MessageGroupFeed from '../components/MessageGroupFeed';
+import {checkAuth, getAccessToken} from '../lib/CheckAuth';
+
+export default function MessageGroupsPage() {
+  const [messageGroups, setMessageGroups] = React.useState([]);
+  const [popped, setPopped] = React.useState([]);
+  const [user, setUser] = React.useState(null);
+  const dataFetchedRef = React.useRef(false);
+
+  const loadData = async () => {
+    try {
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
+      const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${access_token}`
+        },
+        method: "GET"
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setMessageGroups(resJson)
+      } else {
+        console.log(res)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };  
+
+  React.useEffect(()=>{
+    //prevents double call
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+
+    loadData();
+    checkAuth(setUser);
+  }, [])
+  return (
+    <article>
+      <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
+      <section className='message_groups'>
+        <MessageGroupFeed message_groups={messageGroups} />
+      </section>
+      <div className='content'>
+      </div>
+    </article>
+  );
+}
