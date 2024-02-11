ee4c299a7cb939b5cf23dbb2e2861117a57fee72
Bhanu Malhotra <bhanumalhotra219@gmail.com>
2024-02-11 07:27:59 +0000

    wrote code for s3 used cdk synth to check

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ThumbingServerlessCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const bucket = this.createBucket(bucketName)
+   
   }
+
+  createBucket (bucketName: string): s3.IBucket {
+    const bucket = new s3.Bucket(this, 'ThumbingBucket',{
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    return bucket;
+  }
+    
 }
