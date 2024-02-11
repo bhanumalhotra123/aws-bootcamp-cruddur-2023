15f7bb71629b3e0ff99217b37c8d4d9562599efa
Bhanu Malhotra <bhanumalhotra219@gmail.com>
2024-02-11 08:31:41 +0000

    wrote code for lambda function, created .env, installed dotenv using npm i dotenv ran cdk synth and we can see lambda function with s3 this time.

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,13 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
+import { LambdaSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
+import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
+
+//load env vars
+const dotenv = require('dotenv')
+dotenv.config();
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
     const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const functioPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+
+
     const bucket = this.createBucket(bucketName)
+    const lambda = this.createLambda(functioPath)
+     
    
   }
 
@@ -17,6 +29,17 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
     return bucket;
+  }
+
+  createLambda(functionPath: string): lambda.IFunction{
+    const lambdaFunction = new lambda.Function(this, 'ThumbLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(functionPath)
+
+    });
+    return lambdaFunction
+
   }
     
 }
