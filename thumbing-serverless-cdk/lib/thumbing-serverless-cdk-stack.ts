320854bd85330aa02545b5792b5376d736d40d1a
Bhanu Malhotra <144083659+bhanumalhotra123@users.noreply.github.com>
2024-02-12 08:45:13 +0000

    fixing things for codespaces dev container and also starting to write our lambda functions for serverless image process

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,40 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-import { LambdaSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
-import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
+import * as dotenv from 'dotenv';
 
-//load env vars
-const dotenv = require('dotenv')
 dotenv.config();
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
+
+    // The code that defines your stack goes here
     const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
-    const functioPath: string = process.env.THUMBING_FUNCTION_PATH as string;
-    const folderInput: string = process.env.THUMBING_S3_INPUT_FOLDER as string;
-    const folderOutput: string = process.env.THUMBING_S3_OUTPUT_FOLDER as string;
+    const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
+    const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
+    const webhookUrl: string = process.env.THUMBING_WEBHOOK_URL as string;
+    const topicName: string = process.env.THUMBING_TOPIC_NAME as string;
+    const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
 
-
-
-    const bucket = this.createBucket(bucketName)
-    const lambda = this.createLambda(functioPath, bucketName, folderInput, folderOutput);
-     
-   
+    console.log('bucketName',bucketName)
+    console.log('folderInput',folderInput)
+    console.log('folderOutput',folderOutput)
+    console.log('webhookUrl',webhookUrl)
+    console.log('topicName',topicName)
+    console.log('functionPath',functionPath)
   }
 
-  createBucket (bucketName: string): s3.IBucket {
-    const bucket = new s3.Bucket(this, 'ThumbingBucket',{
+  createBucket(bucketName: string): s3.IBucket {
+    const bucket = new s3.Bucket(this, 'ThumbingBucket', {
       bucketName: bucketName,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
     return bucket;
   }
 
-  createLambda(functionPath: string, bucketName: string, folderInput: string, folderOutput: string): lambda.IFunction{
+  createLambda(functionPath: string, bucketName: string, folderInput: string, folderOutput: string): lambda.IFunction {
     const lambdaFunction = new lambda.Function(this, 'ThumbLambda', {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'index.handler',
@@ -46,10 +46,8 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
         PROCESS_WIDTH: '512',
         PROCESS_HEIGHT: '512'
       }
-
     });
-    return lambdaFunction
+    return lambdaFunction;
+  } 
 
-  }
-    
 }
