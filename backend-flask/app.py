a82eedfbdb0ce65d10033a5c41cf37219e0f5d4c
Bhanu Malhotra <bhanumalhotra219@gmail.com>
2024-02-28 23:07:52 +0000

    fixes week08 3

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -106,35 +106,19 @@ cors = CORS(
 #    return response
 
 # Rollbar ----------
-rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-        # access token
-        rollbar_access_token,
-        # environment name
-        'production',
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
-
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 @app.route('/api/health-check')
 def health_check():
   return {'success': True}, 200
 
-# @app.route('/rollbar/test')
-# def rollbar_test():
-#     rollbar.report_message('Hello World!', 'warning')
-#     return "Hello World!"
+#@app.route('/rollbar/test')
+#def rollbar_test():
+#    rollbar.report_message('Hello World!', 'warning')
+#    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
   access_token = extract_access_token(request.headers)
-  
   try:
     claims = cognito_jwt_token.verify(access_token)
     # authenicatied request
@@ -241,7 +225,7 @@ def data_notifications():
 def data_handle(handle):
   model = UserActivities.run(handle)
   if model['errors'] is not None:
-    return model['errozrs'], 422
+    return model['errors'], 422
   else:
     return model['data'], 200
 
