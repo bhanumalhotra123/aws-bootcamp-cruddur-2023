e8c140c2cc01079484487304fee9bce07d9d0dbb
Bhanu Malhotra <bhanumalhotra219@gmail.com>
2024-03-30 14:31:20 +0000

    replies wip2

--- backend-flask/lib/rollbar.py
+++ backend-flask/lib/rollbar.py
@@ -1,26 +1,34 @@
-# Rollbar ----------
-import os
-from flask import current_app as app
-
+from flask import got_request_exception
 from time import strftime
 import os
 import rollbar
 import rollbar.contrib.flask
-from flask import got_request_exception
 
+## XXX hack to make request data work with pyrollbar <= 0.16.3
+def _get_flask_request():
+    print("Getting flask request")
+    from flask import request
+    print("request:", request)
+    return request
+rollbar._get_flask_request = _get_flask_request
 
-def init_rollbar():
-    rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-    def init_rollbar():
-    rollbar.init(
-        # access token
-        rollbar_access_token,
-        # environment name
-        'production',
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
+def _build_request_data(request):
+    return rollbar._build_werkzeug_request_data(request)
+rollbar._build_request_data = _build_request_data
+## XXX end hack
 
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+def init_rollbar(app):
+  rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+  flask_env = os.getenv('FLASK_ENV')
+  rollbar.init(
+      # access token
+      rollbar_access_token,
+      # environment name
+      flask_env,
+      # server root directory, makes tracebacks prettier
+      root=os.path.dirname(os.path.realpath(__file__)),
+      # flask already sets up logging
+      allow_logging_basic_config=False)
+  # send exceptions from `app` to rollbar, using flask's signal system.
+  got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+  return rollbar
